dd922eafcf8451d4c7b0b6dd277be3884dd8899e
IagoNicolas <iago.nicolas@ufpe.br>
2022-05-18 21:14:06 -0300

    Atualizando para Arch LTS

--- blocks.h
+++ blocks.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/    /*Command*/		                                            /*Update Interval*/	/*Update Signal*/
-    {" Ram: ",  "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	   2,		           0},
+    {" Ram: ",  "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",     2,		           0},
     {"Net: ",   "iwgetid -r",                                                  5,                  0},
-    {"Bat: ",   "acpi | awk '{print $4}' | sed 's/,//g'",                      5,                  0},
+    {"Bat0: ",   "cat /sys/class/power_supply/BAT0/capacity",                 30,                  0},
+    {"Bat1: ",   "cat /sys/class/power_supply/BAT1/capacity",                 30,                  0},
     {"Vol: ",   "pamixer --get-volume",                                        2,                  0},
     {"Bri: ",   "xbacklight -get | awk '{print $1}' | sed 's/\\(\\.\\)[0-9]*//g'",                2,                  0},
 	{"",        "date '+%b %d (%a) %H:%M'",					               60,		           0},
